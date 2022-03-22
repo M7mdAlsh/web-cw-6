339f825312a6ab05a29d2e1c57ed5894bec7a7b4
M7mdAlsh <100633304+M7mdAlsh@users.noreply.github.com>
2022-03-22 06:41:17 +0300

    done

---
+++ Exercise 1/ex1.js
@@ -0,0 +1,12 @@
+function travel(x = "زمردة"){
+// console.log('اود السفر الى كوكب')
+}
+
+let planet = prompt("اي كوكب تبي تسافر؟؟")
+
+travel(planet);
+travel();
+
+(function travel (x = 'زمردة') {
+console.log(' اود السفر الى كوكب ' + x)
+})(planet);
